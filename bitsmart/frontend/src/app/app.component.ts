12eaa55676dec8a018956408c42f9673d3c05878
YongenChen <yongen.chen@sjsu.edu>
2024-04-27 17:40:48 -0700

    added header and date selection functionality

--- bitsmart/frontend/src/app/app.component.ts
+++ bitsmart/frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatePicker } from './components/DatePicker/DatePicker.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePicker],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // template: `<div><datepicker></datepicker></div>`,
 })
 export class AppComponent {
   title = 'Bitcoin?';
